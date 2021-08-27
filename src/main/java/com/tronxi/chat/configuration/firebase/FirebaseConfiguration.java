fa52723bab7a682466c4edef876c3a8022033c31
Sergio García Sánchez <sergio.gsanchez97@gmail.com>
2021-08-27 03:45:58 +0200

    fix

--- src/main/java/com/tronxi/chat/configuration/firebase/FirebaseConfiguration.java
+++ src/main/java/com/tronxi/chat/configuration/firebase/FirebaseConfiguration.java
@@ -3,6 +3,9 @@ package com.tronxi.chat.configuration.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -20,15 +23,17 @@ public class FirebaseConfiguration {
    @PostConstruct
     public void initialize() {
         try {
+            JSONParser jsonParser = new JSONParser();
             String firebaseConfigWithSpaces = firebaseConfig.replace("#", " ");
-            System.out.println(firebaseConfigWithSpaces);
-            InputStream stream = new ByteArrayInputStream(firebaseConfigWithSpaces.getBytes());
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(firebaseConfigWithSpaces);
+            System.out.println(jsonObject.toJSONString());
+            InputStream stream = new ByteArrayInputStream(jsonObject.toJSONString().getBytes());
             FirebaseOptions options = new FirebaseOptions
                     .Builder()
                     .setCredentials(GoogleCredentials.fromStream(stream))
                     .build();
             FirebaseApp.initializeApp(options);
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
    }
